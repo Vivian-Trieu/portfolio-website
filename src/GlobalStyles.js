b5d8a861c8ea78f228dc625e0b3ab68f8f066193
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-08-21 05:58:15 -0700

    custom cursor

---
+++ src/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { Global } from "@emotion/react";
+import cursor from "./assets/normal.cur";
+import hoverCursor from "./assets/hover.cur";
+
+const GlobalStyles = () => (
+    <Global
+        styles={`
+            body {
+                cursor: url(${cursor}), auto;
+            }
+            a, button {
+                cursor: url(${hoverCursor}), pointer;
+            }
+        `}
+    />
+);
+
+export default GlobalStyles;
