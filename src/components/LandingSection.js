be926317eecf7afb690c4c6f7cc84bad34d0ab94
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-08-21 05:59:45 -0700

    changed bio

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -7,7 +7,7 @@ import sparkle from "../assets/sparkle.svg";
 
 const greeting = "Hi, I'm Vivian!";
 const title = "UI/UX designer + front-end developer";
-const bio = "Driven by a passion for design and a talent for creating elegant user experiences, I combine creativity with front-end coding expertise to bring visions to life.";
+const bio = "I have a passion for visual design that is both aesthetically pleasing and functional. This has inspired me to build a skill set that can nurture my design skills with technical front-end development.";
 
 
 // Implement the UI for the LandingSection component according to the instructions.
@@ -24,18 +24,18 @@ const LandingSection = () => (
     backgroundPosition="center"
   >
     <Box position="relative">
-      <Image src={sparkle} position="absolute" left="-130px" bottom="70px" transform="scaleY(-1)" boxSize="80px"/>
+      <Image src={sparkle} position="absolute" left="-90px" bottom="70px" transform="scaleY(-1)" boxSize="80px"/>
       <Image src={sparkle} position="absolute" right="560px" top="70px" transform="scaleX(-1)" boxSize="80px"/>
-      <Image src={sparkle} position="absolute" right="60px" bottom="-20px" transform="scaleY(-1)" boxSize="80px"/>
+      <Image src={sparkle} position="absolute" right="80px" bottom="10px" transform="scaleY(-1) scaleX(-1)" boxSize="80px"/>
 
-      <HStack spacing={20}>
+      <HStack spacing={15} padding={10}>
         <VStack spacing={6} alignItems="flex-start">
-          <Text as="i" fontSize="2xl" >{title}</Text>
-          <Heading as="h1" size="4xl" >{greeting}</Heading>
-          <Text fontSize="3xl">{bio}</Text>
+          <Text as="i" fontSize="xl" >{title}</Text>
+          <Heading as="h1" size="3xl" >{greeting}</Heading>
+          <Text fontSize="2xl">{bio}</Text>
         </VStack>
         <Image
-          size="3xl"
+          size="xl"
           src={portrait}
         />
       </HStack>
